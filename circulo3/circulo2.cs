7eb3b8938e16c09961bafe183b7e55592bf530e0
cristian ermes morales loaiza <christianjack01@hotmail.com>
2019-08-30 19:34:59 -0500

    calculo area del circlo

---
+++ circulo3/circulo2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace circulo3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnenviar_Click(object sender, EventArgs e)
+        {
+            double area;
+            area=Math.PI*double.Parse(txtradio.Text)*double.Parse(txtradio.Text);
+            lbltotal.Text= area.ToString();
+        }
+    }
+}
---
+++ circulo3/circulo2.designer.cs
@@ -0,0 +1,120 @@
+﻿namespace circulo3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtradio = new System.Windows.Forms.TextBox();
+            this.btnenviar = new System.Windows.Forms.Button();
+            this.lblresult = new System.Windows.Forms.Label();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "calculador de area de un circulo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "inserte el area del circulo";
+            // 
+            // txtradio
+            // 
+            this.txtradio.Location = new System.Drawing.Point(168, 64);
+            this.txtradio.Name = "txtradio";
+            this.txtradio.Size = new System.Drawing.Size(100, 20);
+            this.txtradio.TabIndex = 2;
+            // 
+            // btnenviar
+            // 
+            this.btnenviar.Location = new System.Drawing.Point(94, 127);
+            this.btnenviar.Name = "btnenviar";
+            this.btnenviar.Size = new System.Drawing.Size(75, 23);
+            this.btnenviar.TabIndex = 3;
+            this.btnenviar.Text = "enviar radio";
+            this.btnenviar.UseVisualStyleBackColor = true;
+            this.btnenviar.Click += new System.EventHandler(this.btnenviar_Click);
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.Location = new System.Drawing.Point(31, 163);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(50, 13);
+            this.lblresult.TabIndex = 4;
+            this.lblresult.Text = "resultado";
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Location = new System.Drawing.Point(88, 163);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(13, 13);
+            this.lbltotal.TabIndex = 5;
+            this.lbltotal.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lbltotal);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.txtradio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnenviar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtradio;
+        private System.Windows.Forms.Button btnenviar;
+        private System.Windows.Forms.Label lblresult;
+        private System.Windows.Forms.Label lbltotal;
+    }
+}
+
